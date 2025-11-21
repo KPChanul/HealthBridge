48118d8b24f5c5c1a3546ff24df076165fa09cb4
Nimeth Sandive <insranaweera@gmail.com>
2025-11-22 01:01:10 +0530

    FeedBack.jsx and Feedback.css edited

--- HealthBridgeWeb/src/components/Feedback-Form/FeedBack.jsx
+++ HealthBridgeWeb/src/components/Feedback-Form/FeedBack.jsx
@@ -1,25 +1,56 @@
+import "./Feedback.css";
+
 export default function ContactUsForm() {
   return (
-    <div className="contact-container">
-      <h2>Contact Us</h2>
+    <div className="feedback-container">
+      <h2 id="feedback-h2">Send Us Your Feedback</h2>
+      <form action="#" method="POST">
+        <div className="form-group">
+          <label for="name">Full Name : </label>
+          <input type="text" id="name" name="name" required />
+        </div>
 
-      <form>
+        <div className="form-group">
+          <label for="email">Email Address : </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@email.com"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="role">You are a</label>
+          <select id="role" name="role" required>
+            <option value="">-- Select your role --</option>
+            <option value="donor">Donor</option>
+            <option value="patient">Patient</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
 
-        <label>Name</label>
-        <input type="text" name="name" placeholder="Enter your name" required />
+        <div className="form-group">
+          <label for="type">Feedback Type</label>
+          <select name="type" id="type" required>
+            <option value="">-- Select --</option>
+            <option value="issue">Report an Issue</option>
+            <option value="suggestion">Suggestion</option>
+            <option value="general">General Comment</option>
+          </select>
+        </div>
 
-        <label>Email</label>
-        <input type="email" name="email" placeholder="Enter your email" required />
+        <div class="form-group">
+          <label for="message">Your Message : </label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Write your feedback here..."
+            required
+          ></textarea>
+        </div>
 
-        <label>Message</label>
-        <textarea
-          name="message"
-          rows="4"
-          placeholder="Write your message..."
-          required
-        ></textarea>
-
-        <button type="submit">Send Message</button>
+        <button type="submit">Submit Feedback</button>
       </form>
     </div>
   );
