cba299d644e39a75c775f838eb13b04a5a949bec
K.P. Chanul Mindipa <kpchanul@gmail.com>
2025-11-21 16:24:14 +0530

    changes

--- HealthBridgeWeb/src/pages/contacts.jsx
+++ HealthBridgeWeb/src/pages/contacts.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from "../components/header/Header"
 
 
+
 function Contacts(){
     return(
-    <>
+    <main >
         <Header/>
         <h1>Contact us</h1>
-    </>
+    </main>
     )
 }
 
