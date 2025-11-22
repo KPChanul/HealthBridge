fc1261ccdcc78cd4391f6f0f335ddabb227249fd
NishakaraNirmal <nirmalnishakara@gmail.com>
2025-11-22 15:32:44 +0530

    half home
    create half of home

--- HealthBridgeWeb/src/pages/home.jsx
+++ HealthBridgeWeb/src/pages/home.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import Header from "../components/header/Header"
-import mainImage from '../assets/main.jpg'
+import mainImage from '../assets/main3.jpg'
+import mainImage2 from '../assets/main4.webp'
+import mainImage3 from '../assets/main5.avif'
+import '../pagesCSS/home.css'
 
 function Home(){
     return(
     
     <>
         <Header/>
-        <h1>Home Page</h1>
+        <div className='contImg'>
+            <div className='imageContainer'>
+                <img className='mainImage' src={mainImage} alt="main home image1" />
+                <img className='mainImage' src={mainImage2} alt="main home image2" />
+                <img className='mainImage' src={mainImage3} alt="main home image3" />
+            </div>
+        </div>
+
+        <div className='headTopic'>
+            <div className='homeTopic'>
+                <h1 className='topic'>Investing in Wellness, Building Healthier Futures</h1>
+            </div>
+        </div>
+        
     </>
     
     )
