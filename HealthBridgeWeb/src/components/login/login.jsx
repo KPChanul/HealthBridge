4df574c8b5a2e1354e23ab445ba8252c2d3851a2
K.P. Chanul Mindipa <kpchanul@gmail.com>
2025-11-22 22:36:23 +0530

    making login form 2

--- HealthBridgeWeb/src/components/login/login.jsx
+++ HealthBridgeWeb/src/components/login/login.jsx
@@ -11,17 +11,17 @@ function Login() {
                     Username:
                     <input type="text" className={styles.input} />
                 </label>
-
+                
                 <label className={styles.label}>
                     Password:
                     <input type="password" className={styles.input} />
                 </label>
                 
-
+                <label className={styles.errorMessage} id="errorMessage">{}</label>
                 <button type="submit" className={styles.loginButton}>Submit</button>
                 
             </form>
-            <label className={styles.errorMessage} id="errorMessage"></label>
+            
         </div>
     );
 }
