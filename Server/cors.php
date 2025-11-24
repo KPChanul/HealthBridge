77e37ebe7e7960eb61799b2648821b8f4693bc5b
K.P. Chanul Mindipa <kpchanul@gmail.com>
2025-11-24 19:44:40 +0530

    Update cors.php

--- Server/cors.php
+++ Server/cors.php
@@ -8,7 +8,7 @@ if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === $frontendOrigi
     header("Access-Control-Allow-Origin: $frontendOrigin");
 }
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: *");
 
 
 // Reply to preflight and stop further processing
