fa4b97e592bd3837c52c4b4ffbbf639404b7d4c0
K.P. Chanul Mindipa <kpchanul@gmail.com>
2025-12-03 12:10:06 +0530

    Update donations.jsx

--- HealthBridgeWeb/src/pages/donations.jsx
+++ HealthBridgeWeb/src/pages/donations.jsx
@@ -2,15 +2,3 @@ import React from 'react'
 import Header from "../components/header/Header"
 
 
-function Donations(){
-    return(
-    <>
-
-        <Header/>
-        <h1>Donations</h1>
-
-    </>
-    )
-}
-
-export default Donations
