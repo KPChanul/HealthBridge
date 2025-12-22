cea35ff23029cfe8226f79801089659a090dc336
K.P. Chanul Mindipa <kpchanul@gmail.com>
2025-12-22 21:22:03 +0530

    Update sysadmin.interface.jsx

--- HealthBridgeWeb/src/pages/sysadmin.interface.jsx
+++ HealthBridgeWeb/src/pages/sysadmin.interface.jsx
@@ -1,6 +1,7 @@
 import { useContext,useEffect, useState } from "react";
 import axios from "axios";
-import '../pagesCSS/sysAdmin.css'
+import '../pagesCSS/sysAdmin.css';
+import { AdminContext } from '../admin.jsx';
 
 // Define the API URL once
 const API_URL = 'http://localhost/serverHB/sysAdmin.php';
